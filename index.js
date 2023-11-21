f3a49c25bbc5fd93c65cb55ab47ac2fe21039221
Vikram <srivikram02@gmail.com>
2023-11-21 14:31:26 +0530

    correct format

--- index.js
+++ index.js
@@ -10,9 +10,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 const jwt_auth=require('./auth/authvadalation')
 const cookieParser = require("cookie-parser");
 app.use(cookieParser())
-
-
-
 app.use(express.json())
 
 app.use('/try',router)
@@ -20,13 +17,9 @@ app.use('/',jwt_auth)
 app.use('/exe',authrouter)
 
 
-const uri =`mongodb://127.0.0.1:27017/?readPreference=primary&appname=MongoDB%20/karthi`;
-Mongoose.connect(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-
-const PORT = process.env.PORT || 5000;
+const uri =`mongodb://localhost:27017`;
+Mongoose.connect(uri);
+const PORT = process.env.PORT || 5035;
   app.listen(PORT, () => {
-    console.log(`Port is running`);
+    console.log(`Port is running :`+" " + PORT);
   });
